bf9db8d3f9664588f237914f260b2d67f7797010
Fang <ibrahim.silver20@gmail.com>
2021-07-24 19:53:02 +0530

    Added assets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Weather from "./components/weather";
 
 ReactDOM.render(<App />, document.getElementById("root"));
